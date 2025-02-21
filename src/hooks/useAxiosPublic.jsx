5b4fd45487dffc75fa6854f3145af9c97c1404aa
Maksudul_Haque <smmaksudulhaque2000@gmail.com>
2025-02-21 12:40:05 +0600

    Finishing Touch Pro

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://task-pilot-server-taupe.vercel.app",
 });
 
 const useAxiosPublic = () => {
